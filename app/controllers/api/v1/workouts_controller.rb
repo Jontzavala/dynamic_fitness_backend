a1d941f5c6d7ce258e0c620186c389dc19e4d1e1
Jonathan Zavala <jontzavala@gmail.com>
2021-12-11 14:25:49 -0600

    worked on workouts controller

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -2,40 +2,43 @@ class Api::V1::WorkoutsController < ApplicationController
   before_action :set_workout, only: [:show, :update, :destroy]
 
   # GET /workouts
-  def index
-    @workouts = Workout.all
+  # def index
+  #   @workouts = Workout.all
 
-    render json: @workouts
-  end
+  #   render json: @workouts
+  # end
 
-  # GET /workouts/1
-  def show
-    render json: @workout
-  end
+  # # GET /workouts/1
+  # def show
+  #   render json: @workout
+  # end
 
   # POST /workouts
   def create
-    @workout = Workout.new(workout_params)
+    @gym = Gym.find_by_id(params[:gym_id])
+    @workout = @gym.workouts.create(workout_params)
 
     if @workout.save
-      render json: @workout, status: :created, location: @workout
+      render json: GymSerializer.new(@gym), status: :created
     else
       render json: @workout.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /workouts/1
-  def update
-    if @workout.update(workout_params)
-      render json: @workout
-    else
-      render json: @workout.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @workout.update(workout_params)
+  #     render json: @workout
+  #   else
+  #     render json: @workout.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /workouts/1
   def destroy
     @workout.destroy
+    gym = Gym.find_by_id(params[:gym_id])
+    render json: GymSerializer.new(gym)
   end
 
   private
